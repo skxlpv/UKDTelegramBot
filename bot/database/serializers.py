89cd2441244f70215dbf649685f6a370a19f807a
skxlpv <olexandertym0000@gmail.com>
2023-03-24 21:04:43 +0200

    DB-6: Added test handlers for database methods testing

--- bot/database/serializers.py
+++ bot/database/serializers.py
@@ -1,6 +1,6 @@
 from bot.utils.api_requests import departments
-from connection import get_schedule_picked as get_collection
-from schedule_requests import get_from_collection
+from bot.database.connection import get_schedule_picked as get_collection
+# from bot.database.schedule_requests import get_from_collection
 
 
 def process_text(group_name, isTeacher=False):
@@ -26,6 +26,17 @@ def validate_favorites_quantity(user):
     return 1
 
 
+# ////////////////////////////////////////////////////////////////////////
+def get_from_collection(user, action):
+    col = get_collection()
+    query = {'user_id': user, f"{action}": {"$exists": True}}
+    result = col.find_one(query)
+    if not result:
+        return -20
+    return result[f'{action}']
+
+
+# ////////////////////////////////////////////////////////////////////////
 def get_insert_data(group_name, group_id, isTeacher=False):
     if not isTeacher:
         insert_data = {'group_id': group_id,
