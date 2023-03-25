afba135fa210e190c06c11635f61b0b15ed13344
skxlpv <olexandertym0000@gmail.com>
2023-03-25 13:53:46 +0200

    FIX-1: Fixed conflict between schedule display and schedule search handlers. Fixed keyboard lags after click in mobile Telegram

--- bot/keyboards/reply/specialties_keyboard.py
+++ bot/keyboards/reply/specialties_keyboard.py
@@ -1,3 +1,3 @@
 from aiogram.types.reply_keyboard import ReplyKeyboardMarkup
 
-specialties_keyboard = ReplyKeyboardMarkup(row_width=1, one_time_keyboard=True)
+specialties_keyboard = ReplyKeyboardMarkup(row_width=1)
