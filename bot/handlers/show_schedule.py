3af1e7f838ac33d2e8572a42e64619a1221df44c
skxlpv <olexandertym0000@gmail.com>
2023-03-25 13:53:38 +0200

    FIX-1: Fixed conflict between schedule display and schedule search handlers. Fixed keyboard lags after click in mobile Telegram

--- bot/handlers/show_schedule.py
+++ bot/handlers/show_schedule.py
@@ -1,39 +1,41 @@
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Dispatcher, types
 import requests
 import time
+
+from aiogram.dispatcher.filters import CommandStart
+
+from bot.handlers.search import search_schedule
 from loader import bot, dp
 from bot.keyboards.inline.schedule_keyboard import schedule_keyboard
 from bot.keyboards.reply.menu_keyboard import menu_keyboard
 from bot.utils import schedule_utils
 
-@dp.message_handler(commands=['start'])
+
+@dp.message_handler(CommandStart())
 async def start(message: types.Message):
-    await bot.send_message(chat_id = message.from_user.id,
-                         text=f'Вітаю,{message.from_user.first_name} {message.from_user.last_name}!\n '
-                             f'Я -- офіційний бот-асистент від Університету Короля Данила!', parse_mode='html')
-    await message.delete()
-    await bot.send_message(chat_id = message.from_user.id, text = 'Будь ласка, виберіть бажану опцію',
+    await bot.send_message(chat_id=message.from_user.id,
+                           text=f'Вітаю, {message.from_user.first_name}!\n'
+                                f'Я -- офіційний бот-асистент від Університету Короля Данила!\n'
+                                f'Будь ласка, виберіть бажану опцію',
                            reply_markup=menu_keyboard)
 
 
-@dp.message_handler(regexp = "Мій розклад")
+@dp.message_handler(text=["Знайти розклад", "Мій розклад", "Обране"])
 async def first_answer(message: types.Message):
     if message.text == 'Знайти розклад':
-        await bot.send_message(chat_id = message.from_user.id, text ="hello:)")
-        await message.delete()
+        await search_schedule(message=message)
+        # await GeneralStates.search_schedule.set()
     elif message.text == 'Мій розклад':
-        time_str = time.strftime("%d.%m.%Y")
-        url = f'http://195.162.83.28/cgi-bin/timetable_export.cgi?req_type=rozklad&req_mode=group&OBJ_ID=&OBJ_name=%B2%CF%C7%F1-21-2&dep_name=&ros_text=separated&begin_date={time_str}&end_date={time_str}&req_format=json&coding_mode=UTF8&bs=ok'
-        data = requests.get(url).json()
-        await my_schedule(message, data)
-        await message.delete()
+        await my_schedule(message)
     elif message.text == 'Обране':
-        await bot.send_message(chat_id = message.from_user.id, text ="papa:)")
-        await message.delete()
+        await bot.send_message(chat_id=message.from_user.id, text="Favorites is not implemented")
 
 
-@dp.message_handler()
-async def my_schedule(message: types.Message, data):
+async def my_schedule(message: types.Message):
+    time_str = time.strftime("%d.%m.%Y")
+    url = f'http://195.162.83.28/cgi-bin/timetable_export.cgi?req_type=rozklad&req_mode=group&OBJ_ID=&OBJ_name=%B2%CF%C7%F1-21-2&dep_name=&ros_text=separated&begin_date={time_str}&end_date={time_str}&req_format=json&coding_mode=UTF8&bs=ok'
+    data = requests.get(url).json()
+
     data = data['psrozklad_export']['roz_items']
     schedule_list = []
     for i in data:
@@ -61,14 +63,12 @@ async def my_schedule(message: types.Message, data):
                            reply_markup=schedule_keyboard)
 
 
-@dp.message_handler()
-async def day_schedule(callback: types.CallbackQuery):
-    pass
+@dp.message_handler()   # temporary solution for incorrect input handling
+async def incorrect_input(message: types.Message):
+    await message.answer('Некоректний ввід!')
 
 
 def register_schedule_handlers(dispatcher: Dispatcher):
     dispatcher.register_message_handler(start)
     dispatcher.register_message_handler(first_answer)
     dispatcher.register_message_handler(my_schedule)
-    dispatcher.register_message_handler(day_schedule)
-
