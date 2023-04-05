d057bf57d059d27a1e084572678d3b0d413d68b6
palibrix <theopaolo04@gmail.com>
2023-04-05 19:47:37 +0300

    DB-14: fully implemented docker connection, added mongo express to manage database

--- bot/database/__init__.py
+++ bot/database/__init__.py
@@ -1,9 +1,13 @@
-import logging
-
 from pymongo import MongoClient
 
-from configs import CONNECTION_STRING, MONGO_HOST, MONGO_PORT, MONGO_PASS, MONGO_USER, MONGO_DB
+from configs import MONGO_HOST, MONGO_PORT, MONGO_PASS, MONGO_USER
 
+client = MongoClient(host=MONGO_HOST,
+                     port=int(MONGO_PORT),
+                     username=MONGO_USER,
+                     password=MONGO_PASS,
+                     authSource="admin")
+db = client['user']
 
-client = MongoClient(CONNECTION_STRING)
-
+db['schedule_picked'].create_index('user_id', unique=True)
+db['user_preferences'].create_index('user_id', unique=True)
--- bot/database/connection.py
+++ bot/database/connection.py
@@ -1,21 +1,15 @@
 from dotenv import load_dotenv
 
-from bot.database import client
+from bot.database import db
 
 load_dotenv()
 
 
-def get_database():
-    return client['user']
-
-
 def get_schedule_picked():
-    client = get_database()
-    col = client['schedule_picked']
+    col = db['schedule_picked']
     return col
 
 
 def get_user_pref():
-    client = get_database()
-    col = client['user_preferences']
+    col = db['user_preferences']
     return col
