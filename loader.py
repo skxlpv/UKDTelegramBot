dd00b69c5ca114525898be0d89d6b94c0d798711
skxlpv <olexandertym0000@gmail.com>
2023-04-07 20:21:17 +0300

    LOG-1: set up logger

--- loader.py
+++ loader.py
@@ -3,13 +3,27 @@ import logging
 from aiogram import Bot
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import Dispatcher
-from aiogram.types import BotCommand
 
 from bot.worker.scheduler import scheduler
 from configs import API_TOKEN
 
 # Configure logging
-logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('UKD_bot')
+logger.setLevel(logging.FATAL)
+
+file_handler = logging.FileHandler('UKD_bot.log')
+file_handler.setLevel(logging.INFO)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+
+log_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
+
+file_handler.setFormatter(log_formatter)
+stream_handler.setFormatter(log_formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
 # Initialize bot and dispatcher
 storage = MemoryStorage()
